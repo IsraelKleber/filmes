25d5745f6e16d0cf80f9e60266e9c12b2d9db794
Israel Kleber <israel.lima@escolar.ifrn.edu.br>
2024-02-22 19:57:47 -0300

    feat: adiciona o componente CardMovie (#3)

---
+++ freemovies/src/components/cardMovie.tsx
@@ -0,0 +1,40 @@
+import { Movie } from "@/interface/CardMovieInterface";
+import { Card, CardContent } from "./ui/card";
+
+interface CardMovieProps {
+  movies: Movie[];
+  className?: string;
+}
+
+function CardMovie({ movies, className }: CardMovieProps): JSX.Element {
+  return (
+    <div className={className}>
+      {/* Adicione esta linha */}
+      {movies.map((movie) => (
+        <Card className="min-w-[150px] max-w-[200px] rounded-2xl flex flex-col m-4 hover:bg-sky-600 hover:border-sky-800">
+          <CardContent className="py-0 pt-1 px-1">
+            <div key={movie.id}>
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={movie.title}
+                className="h-full object-cover w-full rounded-2xl"
+              />
+              <h2 className="text-lg font-bold overflow-ellipsis overflow-hidden whitespace-nowrap">
+                {movie.title}
+              </h2>
+
+              <div className=" flex flex-col">
+                <span className="text-sm">Nota: {movie.vote_average}</span>
+                <span className="text-sm">
+                  Popularidade: {movie.popularity}
+                </span>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  );
+}
+
+export default CardMovie;
--- freemovies/src/pages/home/index.tsx
+++ freemovies/src/pages/home/index.tsx
@@ -1,23 +1,7 @@
 import { useEffect, useState } from "react";
 import movieService from "../../service/api/movieService";
-import { Card, CardContent } from "@/components/ui/card";
-
-interface Movie {
-  adult: boolean;
-  backdrop_path: string;
-  genre_ids: number[];
-  id: number;
-  original_language: string;
-  original_title: string;
-  overview: string;
-  popularity: number;
-  poster_path: string;
-  release_date: string;
-  title: string;
-  video: boolean;
-  vote_average: number;
-  vote_count: number;
-}
+import { Movie } from "@/interface/CardMovieInterface";
+import CardMovie from "@/components/cardMovie";
 
 function Home() {
   const [movies, setMovies] = useState<Movie[]>([]);
@@ -43,32 +27,7 @@ function Home() {
       <h1 className="text-2xl font-bold flex justify-center">
         Filmes populares
       </h1>
-      <div className="flex flex-wrap justify-center">
-        {/* Adicione esta linha */}
-        {movies.map((movie) => (
-          <Card className="min-w-[150px] max-w-[200px] rounded-2xl flex flex-col m-4">
-            <CardContent className="py-0 pt-1 px-1">
-              <div key={movie.id}>
-                <img
-                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                  alt={movie.title}
-                  className="h-full object-cover w-full rounded-2xl"
-                />
-                <h2 className="text-lg font-bold overflow-ellipsis overflow-hidden whitespace-nowrap">
-                  {movie.title}
-                </h2>
-
-                <div className=" flex flex-col">
-                  <span className="text-sm">Nota: {movie.vote_average}</span>
-                  <span className="text-sm">
-                    Popularidade: {movie.popularity}
-                  </span>
-                </div>
-              </div>
-            </CardContent>
-          </Card>
-        ))}
-      </div>
+      <CardMovie movies={movies} className="flex flex-wrap justify-center" />
     </>
   );
 }
