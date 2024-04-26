f63d38a08ea30743881f9976fcff3d117b3efda4
Israel Kleber <94148869+IsraelKleber@users.noreply.github.com>
2024-04-26 12:10:34 +0000

    feat: adiciona código duplicado para atividade de qualidade de software (#28)

--- freemovies/src/components/cardMovie.tsx
+++ freemovies/src/components/cardMovie.tsx
@@ -36,6 +36,26 @@ function CardMovie({ movies, className }: CardMovieProps): JSX.Element {
 								</div>
 							</div>
 						</CardContent>
+						<Card className="min-w-[150px] max-w-[200px] rounded-2xl flex flex-col m-4 hover:bg-sky-600 hover:border-sky-800">
+						<CardContent className="py-0 pt-1 px-1">
+							<div key={movie.id}>
+								<img
+									src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+									alt={movie.title}
+									className="h-full object-cover w-full rounded-2xl"
+								/>
+								<h2 className="text-lg font-bold overflow-ellipsis overflow-hidden whitespace-nowrap">
+									{movie.title}
+								</h2>
+
+								<div className=" flex flex-col text-sm font-serif ">
+									<span>Nota: {movie.vote_average}</span>
+									<span>
+										Popularidade: {movie.popularity}
+									</span>
+								</div>
+							</div>
+						</CardContent>
 					</Card>
 				</Link>
 			))}
